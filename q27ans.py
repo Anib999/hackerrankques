6db1adfc769737ffc214dceb277f67d8d616dbc2
Anib <saliv.mah@gmail.com>
2022-09-28 21:48:29 +0545

    minion

--- q27ans.py
+++ q27ans.py
@@ -1,5 +1,21 @@
 def minion_game(string):
     # your code goes here
+    vowels = ['A', 'E', 'I', 'O', 'U']
+    # print(string.upper())
+    s = 0
+    k = 0
+    for i in range(len(string)):
+        if string[i] in vowels:
+            k += len(string) - i
+        else:
+            s += len(string) - i
+    if(k>s):
+        print('Kevin', k)
+    elif(s==k):
+        print('Draw')
+    else:
+        print('Stuart', s)
+
 
 if __name__ == '__main__':
     s = input()
---
+++ q28ans.py
@@ -0,0 +1,11 @@
+def merge_the_tools(string, k):
+    # your code goes here
+    stringArr = list(string)
+    for i in range(0, len(stringArr), k):
+        yield stringArr[i:i + k]
+    
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    x = list(merge_the_tools(string, k))
+    print(x)
